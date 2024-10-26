21abbed024bf3f161484b8603129553980e5a50f
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-26 18:49:31 -0500

    feat: add tagged template parser

---
+++ lib.js
@@ -0,0 +1,8 @@
+export function html([first, ...strings], ...values) {
+    return values
+        .reduce((acc, curr) => acc.concat(curr, strings.shift()), [first])
+        .filter(x => x && x !== true || x === 0)
+        .join("");
+};
+
+// export add

429b8f026b2a37f2bba617601bbd7b3522bbe8f4
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-26 18:05:31 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div class="m-10 p-10">
+    <span class="text-red-300 font-semibold"> Red text</span>
+    hello darkness my friend, I come to say hullo!
+  </div>
+`;

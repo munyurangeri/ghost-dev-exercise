789e61a1e8dd94bae3ca6bbd2a2de306ce58412c
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-27 10:44:40 -0500

    feat (primary button): add event listener

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,21 +1,23 @@
-import { html } from "../lib";
+import { html, delegateEvent } from "../lib";
 
 const PrimaryButton = ({
   id,
+  selector,
+  label,
   tag,
   href,
-  label,
   callback,
-  className = "bg-[#FFC840] text-[#262861]",
+  className = "bg-[#FFC840] text-[#262861] hover:shadow-lg",
 }) => {
-
-  const classes = `flex gap-2 px-[32px] py-[16px] rounded-3xl font-mono text-base font-medium leading-[1.125] ${className}`;
-
   const icon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                </svg>`;
-  
-  // TODO: Add event listner if the component tag is button
+    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+  </svg>`;
+
+  const classes = `${selector} flex gap-2 px-[32px] py-[16px] rounded-3xl font-mono text-base font-medium leading-[1.125] ${className}`;
+
+  if (tag.trim() === "button" && callback) {
+    delegateEvent("click", `.${selector}`, callback);
+  }
 
   return tag.trim() === "button"
     ? html`<button id="${id}" class="${classes}">${label} ${icon}</button> `

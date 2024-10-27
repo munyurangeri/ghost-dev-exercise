09876f5ce8f34ce2561797214b4097bd2a255a82
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-27 10:38:11 -0500

    feat: add event deleguation function

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,27 @@
-export function html([first, ...strings], ...values) {
-    return values
-        .reduce((acc, curr) => acc.concat(curr, strings.shift()), [first])
-        .filter(x => x && x !== true || x === 0)
-        .join("");
-};
+const eventListenersRegistry = [];
 
-// export add
+export function delegateEvent(type, selector, callback, parent = document) {
+  parent.addEventListener(type, (event) => {
+    if (event.target.matches(selector)) {
+      callback(event);
+      eventListenersRegistry.push({ parent, type, callback });
+    }
+  });
+}
+
+export function clearEventListners() {
+  eventListenersRegistry.forEach(({ element, type, callback }) => {
+    element.removeEventListener(type, callback);
+  });
+
+  eventListenersRegistry.length = 0;
+}
+
+window.addEventListener("beforeunload", clearEventListners);
+
+export function html([first, ...strings], ...values) {
+  return values
+    .reduce((acc, curr) => acc.concat(curr, strings.shift()), [first])
+    .filter((x) => (x && x !== true) || x === 0)
+    .join("");
+}

5b58a763184d590a385b32dfa6cd3153458832a4
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-28 18:14:31 -0500

    fix (story card): responsiveness

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -4,14 +4,14 @@ import PrimaryButton from "./PrimaryButton";
 
 const StoryCard = ({}) => {
   const markups = `
-        <div class="xl:flex flex-col w-[18.625rem] xl:h-[24.875rem] rounded">
+        <div class="flex flex-col w-full xl:w-[18.625rem] xl:h-[24.875rem]">
             <div class="hidden xl:flex gap-8 mb-8">
                 ${CardActionButton({ action: "previous" })}          
                 ${CardActionButton({ action: "next" })}
             </div>
-            <div>
+            <div class="w-full px-0 xl:p-0">
                 <h1 class="hidden xl:inline-block text-[46px] font-bold font-sans leading-[43px] tracking-tight mb-6">The Roberts Family's Story</h1>
-                <p class="mb-12 xl:mb-7 text-base font-normal leading-[23px]">
+                <p class="mb-12 lg:mb-7 text-lg md:text-3xl xl:text-base leading-[23px] font-normal ">
                     The circumstances that might leat to homelessness can include 
                     loss of income or transportation,
                     a falling out with loved ones, or an abrupt economic downturn.
@@ -28,11 +28,7 @@ const StoryCard = ({}) => {
                 })}
             </div>
             <div class="flex justify-between mx-2 my-0 xl:hidden">
-                ${CardActionButton({ action: "previous" })}
-                ${CardActionButton({
-                  action: "close",
-                  className: "bg-red-500 text-white hover:shadow-lg",
-                })}
+                ${CardActionButton({ action: "previous" })}                
                 ${CardActionButton({ action: "next" })}
             </div>
         </div>

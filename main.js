09d8265a08b0cb4448f74b94dded169b29d5bae6
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-28 20:03:54 -0500

    chore: remove unused (stories) page

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import "./src/style.css";
 import { html } from "./src/lib";
 
 import Layout from "./src/components/Layout";
-import StoriesPage from "./src/StoriesPage";
 import StoryPage from "./src/StoryPage";
 
 document.querySelector("#app").innerHTML = html`${Layout({

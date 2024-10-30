f8b845ba58e0cfe2f028292b0f57216b0c761377
Herve R. Munyurangeri <rherve@gmail.com>
2024-10-30 09:46:35 -0500

    feat: always show the story text, no animated show/hide

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import { html } from "../lib";
 const Layout = ({ page }) => {
   return html`
     <div
-      class="w-full h-screen p-0 m-0 text-white bg-[#F15C22] lg:bg-gradient-to-r lg:from-[#F15C22] lg:from-0% lg:via-[#F7931D] lg:via-50% lg:to-[#FFC840] lg:to-100%"
+      class="p-0 m-0 text-white bg-[#F15C22] lg:bg-gradient-to-r lg:from-[#F15C22] lg:from-0% lg:via-[#F7931D] lg:via-50% lg:to-[#FFC840] lg:to-100%"
     >
       ${page}
     </div>

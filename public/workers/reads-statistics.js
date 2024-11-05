23464358a54a0dd65f55075774b9c24b1a696662
Herve R. Munyurangeri <rherve@gmail.com>
2024-11-05 15:05:00 -0600

    feat: reads statistics worker

--- public/workers/reads-statistics.js
+++ public/workers/reads-statistics.js
@@ -3,7 +3,12 @@ const BASE_URL = "http://localhost:3000";
 self.onmessage = async function (event) {
   const data = await reads();
 
-  self.postMessage(data);
+  const statistics = {
+    readsPerStory: computeReadsPerStory(data),
+    mostReadStory: computeMostReadStory(data),
+  };
+
+  self.postMessage(statistics);
 };
 
 self.onerror = function (event) {
@@ -24,4 +29,25 @@ async function reads() {
   } catch (error) {
     throw new Error("Something went wrong while fetching reads");
   }
+}
+
+function computeReadsPerStory(rowData) {
+  const data = Object.entries(rowData).map(([index, read]) => read);
+
+  return data.reduce((acc, { story }) => {
+    const key = story.trim();
+    acc[key] = (acc[key] || 0) + 1;
+    return acc;
+  }, {});
+}
+
+function computeMostReadStory(data) {
+  const readsPerStory = computeReadsPerStory(data);
+
+  return Object.entries(readsPerStory).reduce(
+    (max, [story, count]) => {
+      return count > max.count ? { story, count } : max;
+    },
+    { story: null, count: 0 }
+  );
 }

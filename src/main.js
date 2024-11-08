8d2f41e527fbb5d9a60b3daeaf44c16d8a9ea2da
Herve R. Munyurangeri <rherve@gmail.com>
2024-11-07 18:54:35 -0600

    chore: refactoring

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { html } from "./lib";
 
-import Layout from "./components/Layout";
+import Layout from "./Layout";
 import StoryPage from "./StoryPage";
 
 async function renderPage() {
